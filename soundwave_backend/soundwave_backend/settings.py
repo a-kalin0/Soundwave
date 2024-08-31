50ebac21e8de2ce645820d6feea76984d764bc22
Arthur Kalinowska <a.kalinowska@students.ephec.be>
2024-08-31 18:07:51 +0200

    Commit de correction de settings.py

--- soundwave_backend/soundwave_backend/settings.py
+++ soundwave_backend/soundwave_backend/settings.py
@@ -91,27 +91,18 @@ WSGI_APPLICATION = 'soundwave_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if DEBUG:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
-
-else:
-
-    DATABASE_URL = config("DATABASE_URL")
-
-    DATABASES = {
-            "default": dj_database_url.config(default=DATABASE_URL, 
-                                            conn_max_age=1800),
-        }
+}
 
 
 # Password validation
