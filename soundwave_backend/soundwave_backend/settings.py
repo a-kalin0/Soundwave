ad7c38da698ccc4a5d8d29e7c02835dd49ad46c9
Arthur Kalinowska <a.kalinowska@students.ephec.be>
2024-08-31 21:24:51 +0200

    Commit correction pour déploiement back + front sur heroku

--- soundwave_backend/soundwave_backend/settings.py
+++ soundwave_backend/soundwave_backend/settings.py
@@ -152,11 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Inclure les fichiers du build Vue.js
