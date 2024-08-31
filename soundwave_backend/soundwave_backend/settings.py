37ed1e21bc52e57d6509add57d424311c6fae293
Arthur Kalinowska <114411228+a-kalin0@users.noreply.github.com>
2024-08-31 20:39:33 +0200

    Update settings.py

--- soundwave_backend/soundwave_backend/settings.py
+++ soundwave_backend/soundwave_backend/settings.py
@@ -111,12 +111,12 @@ WSGI_APPLICATION = 'soundwave_backend.wsgi.application'
 
 else :'''
 
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL')
-        )
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 
-    }
+}
 
 
 # Password validation
