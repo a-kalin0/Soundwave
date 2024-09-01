8e8e6c84ad4167ffd76b470312cc659f675475bb
Arthur Kalinowska <114411228+a-kalin0@users.noreply.github.com>
2024-09-01 16:53:30 +0200

    Update models.py

--- soundwave_backend/soundnews/models.py
+++ soundwave_backend/soundnews/models.py
@@ -42,18 +42,18 @@ class Article(models.Model):
     
     def get_image(self):
         if self.image:
-            return 'https://soundwave.ovh' + self.image.url
+            return 'https://squid-app-k8cip.ondigitalocean.app' + self.image.url
         return  ''
     
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'https://soundwave.ovh' + self.thumbnail.url
+            return 'https://squid-app-k8cip.ondigitalocean.app' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
                 
-                return 'https://soundwave.ovh' + self.thumbnail.url
+                return 'https://squid-app-k8cip.ondigitalocean.app' + self.thumbnail.url
             else:
                 return ''
     
